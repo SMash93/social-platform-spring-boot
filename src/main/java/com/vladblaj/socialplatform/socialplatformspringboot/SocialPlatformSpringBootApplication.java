cceaf082e8441e61fec06406a9edb44cd22ffaa2
Vlad Blaj <Vlad.Blaj@msg.group>
2018-12-13 15:52:29 +0200

    First commit app running

---
+++ src/main/java/com/vladblaj/socialplatform/socialplatformspringboot/SocialPlatformSpringBootApplication.java
@@ -0,0 +1,14 @@
+package com.vladblaj.socialplatform.socialplatformspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialPlatformSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialPlatformSpringBootApplication.class, args);
+	}
+
+}
+
