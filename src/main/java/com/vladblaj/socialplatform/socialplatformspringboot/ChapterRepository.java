e96e40a2f6a828be32098464be04cc0450cc6500
Vlad Blaj <Vlad.Blaj@msg.group>
2018-12-18 15:16:50 +0200

    After Tests Chapter

--- src/main/java/com/vladblaj/socialplatform/socialplatformspringboot/ChapterRepository.java
+++
@@ -1,8 +0,0 @@
-package com.vladblaj.socialplatform.socialplatformspringboot;
-
-import org.springframework.data.repository.reactive.ReactiveCrudRepository;
-
-public interface ChapterRepository
-	extends ReactiveCrudRepository<Chapter, String> {
-
-}
--- src/main/java/com/vladblaj/socialplatform/socialplatformspringboot/SocialPlatformSpringBootApplication.java
+++ src/main/java/com/vladblaj/socialplatform/socialplatformspringboot/SocialPlatformSpringBootApplication.java
@@ -2,13 +2,21 @@ package com.vladblaj.socialplatform.socialplatformspringboot;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.filter.reactive.HiddenHttpMethodFilter;
 
 @SpringBootApplication
 public class SocialPlatformSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SocialPlatformSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SocialPlatformSpringBootApplication.class, args);
+
+    }
+
+    @Bean
+    HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 
 }
 
